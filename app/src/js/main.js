f2dfd4b96f4e85d2a320f299e84ad4d7b7d17e8f
Tetiana Hrytsaienko <kortatiana89@gmail.com>
2017-10-30 11:02:59 +0200

    Build task

--- app/src/js/main.js
+++ app/src/js/main.js
@@ -1,9 +1,9 @@
-$(document).on('click', '[data-toggle="lightbox"]', function (event) {
-    event.preventDefault();
-    $(this).ekkoLightbox();
-});
-
 // LAZYLOAD PHOTOS
 $(document).ready(() => {
     $('.lazy').Lazy();
+});
+
+$(document).on('click', '[data-toggle="lightbox"]', function (event) {
+    event.preventDefault();
+    $(this).ekkoLightbox();
 });
